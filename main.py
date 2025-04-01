e4b8e5086683e0de3cfc31134b65339d4696b6e5
Taras Yarmolenko <tarik00700@ukr.net>
2025-04-01 20:04:31 +0300

    homework_1

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 
-def print_hello():
-    print("Hello!")
-    
-print_hello()
+def get_days_from_today(date):
+    formated_date = datetime.strptime(date, '%Y-%m-%d').date()
+    current_date = datetime.today().date()
+    return (formated_date - current_date).days
+
+print(get_days_from_today('2025-12-31'))
