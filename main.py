85b78d6c7c3cac897b8b73b65271ccfdc7141e62
Taras Yarmolenko <tarik00700@ukr.net>
2025-04-01 20:28:26 +0300

    homework_1.1

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    formated_date = datetime.strptime(date, '%Y-%m-%d').date()
-    current_date = datetime.today().date()
-    return (formated_date - current_date).days
+    try:
+        formated_date = datetime.strptime(date, '%Y-%m-%d').date()
+        current_date = datetime.today().date()
+        return (formated_date - current_date).days
+    except ValueError:
+        return "Incorrectly entered data. Enter in the format: 'YYYY-MM-DD'"
 
 print(get_days_from_today('2025-12-31'))
