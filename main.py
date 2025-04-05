373e71126f9666fc889dd265519fa735ec0533f5
Taras Yarmolenko <tarik00700@ukr.net>
2025-04-05 20:29:00 +0300

    corrections 1.1

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# from datetime import datetime
+from datetime import datetime
 
-# def get_days_from_today(date):
-#     try:
-#         formatted_date = datetime.strptime(date, '%Y-%m-%d').date()
-#         current_date = datetime.today().date()
-#         return (formatted_date - current_date).days
-#     except ValueError:
-#         return "Incorrectly entered data. Enter in the format: 'YYYY-MM-DD'"
+def get_days_from_today(date):
+    try:
+        formatted_date = datetime.strptime(date, '%Y-%m-%d').date()
+        current_date = datetime.today().date()
+        return (formatted_date - current_date).days
+    except ValueError:
+        return "Incorrectly entered data. Enter in the format: 'YYYY-MM-DD'"
 
-# print(get_days_from_today('2025-12-31'))
+print(get_days_from_today('2025-12-31'))
 
 
 
