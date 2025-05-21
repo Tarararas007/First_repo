08c750ff8b25271cf6f1d04f2b14f645e2d2f61c
Taras Yarmolenko <tarik00700@ukr.net>
2025-05-21 21:27:29 +0300

    homework_2

--- main.py
+++ main.py
@@ -1,26 +1,125 @@
-from datetime import datetime
+'''
+Перше завдання
+'''
 
-def get_days_from_today(date):
+def total_salary(path):
+    total = 0
+    count = 0
     try:
-        formatted_date = datetime.strptime(date, '%Y-%m-%d').date()
-        current_date = datetime.today().date()
-        return (formatted_date - current_date).days
-    except ValueError:
-        return "Incorrectly entered data. Enter in the format: 'YYYY-MM-DD'"
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                parts = line.strip().split(',')
+                if parts[1].isdigit():
+                    try:
+                        salary = float(parts[1])
+                        total += salary
+                        count += 1
+                    except ValueError:
+                        continue
+        average = total / count
+        return f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}"
+    except FileNotFoundError:
+        return f"Файл за шляхом '{path}' не знайдено."
 
-print(get_days_from_today('2025-12-31'))
+print(total_salary('salaries.txt'))
 
+'''
+Друге завдання
+'''
 
+def get_cats_info(path):
+    cats_info =[]
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            for line_number, lines in enumerate(file, start=1):
+                parts = lines.strip().split(',')
+                if len(parts) != 3:
+                    print(f"Попередження: Неправильний формат у рядку {line_number}")
+                    continue
+                cat_id, name, age = parts
+                try:
+                    cat_dict = {
+                        'id': cat_id,
+                        'name': name,
+                        'age': int(age)
+                    }
+                    cats_info.append(cat_dict)
+                    print(cat_dict)
+                except ValueError:
+                    print(f"Попередження: Невірний вік у рядку {line_number}: {age}")
+    except FileNotFoundError:
+        print(f"Помилка: Файл за шляхом '{path}' не знайдено.")
+    return cats_info
 
+get_cats_info('cats.txt')
 
-import random
+'''
+Четверте завдання
+'''
 
-def get_numbers_ticket(min, max, quantity):
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
 
-    if min < 1 or max > 1000 or quantity > (max - min + 1) or quantity < 1:
-        return []
+def add_contact(args, contacts):
+    if len(args) != 2:
+        return 'Error: Enter name and phone number.'
+    name, phone = args
+    name = name.lower()
+    contacts[name] = phone
+    return 'Contact added.'
+
+def change_contact(args, contacts):
+    if len(args) != 2:
+        return 'Error: Enter name and new phone number.'
+    name, phone = args
+    name = name.lower()
+    if name in contacts:
+        contacts[name] = phone
+        return 'Contact updated.'
+    else:
+        return 'Contact not found.'
+
+def show_phone(args, contacts):
+    if len(args) != 1:
+        return 'Error: Enter one name to search.'
+    name = args[0].lower()
+    if name in contacts:
+        return contacts[name]
+    else:
+        return 'Contact not found.'
     
-    numbers = random.sample(range(min, max + 1), quantity)
-    return sorted(numbers)
+def show_all(contacts):
+    if not contacts:
+        return 'No contacts found.'
+    result = []
+    for name, phone in contacts.items():
+        result.append(f'{name}: {phone}')
+    return '\n'.join(result)
 
-print(get_numbers_ticket(10,14,6))
+def main():
+    contacts = {}
+    print("Welcome to assistant bot!")
+    while True:
+        user_input = input('Enter a command: ')
+        command, *args = parse_input(user_input)
+
+        if command in ['close', 'exit']:
+            print('Good bay!')
+            break
+        elif command == 'hello':
+            print('How can i halp you?')
+        elif command == 'add':
+            print(add_contact(args, contacts))
+        elif command == 'change':
+            print(change_contact(args, contacts))
+        elif command == 'phone':
+            print(show_phone(args, contacts))
+        elif command == 'all':
+            print(show_all(contacts))
+        else:
+            print('Invalid command.')
+
+if __name__ == "__main__":
+    main()
